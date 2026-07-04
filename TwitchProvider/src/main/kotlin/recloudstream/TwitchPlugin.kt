e5965bbcf14b8ccba1ff12346d8eacb784f1d3d1
galkasv <16668378+galkasv@users.noreply.github.com>
2026-07-04 19:55:39 +0300

    Update TwitchPlugin.kt

--- TwitchProvider/src/main/kotlin/recloudstream/TwitchPlugin.kt
+++ TwitchProvider/src/main/kotlin/recloudstream/TwitchPlugin.kt
@@ -1,13 +1,13 @@
 package recloudstream
 
-import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
 
 @CloudstreamPlugin
-class TwitchPlugin: BasePlugin() {
-    override fun load() {
-        // All providers should be added in this manner. Please don't edit the providers list directly.
+class TwitchPlugin: Plugin() {
+    override fun load(context: Context) {
+        // Реєструємо тільки наш провайдер (який тепер AMD.online)
         registerMainAPI(TwitchProvider())
-        registerExtractorAPI(TwitchProvider.TwitchExtractor())
     }
 }
