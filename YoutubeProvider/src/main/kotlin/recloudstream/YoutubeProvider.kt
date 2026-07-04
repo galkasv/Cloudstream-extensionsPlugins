a5e9cc8f8281a8aa89b102632170e29e4f9787a4
galkasv <16668378+galkasv@users.noreply.github.com>
2026-07-04 16:33:59 +0300

    Build 3f4a55cb25846ebb9db54cc621bfcd15c93e0e01

---
+++ YoutubeProvider/src/main/kotlin/recloudstream/YoutubeProvider.kt
@@ -0,0 +1,104 @@
+package recloudstream
+
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.loadExtractor
+import org.jsoup.Jsoup
+
+// Назву класу залишаємо YoutubeProvider, щоб робот успішно зібрав файл
+class YoutubeProvider : MainAPI() { 
+    override var mainUrl = "https://animesss.com"
+    override var name = "Animesss" // А в самому додатку CloudStream назва буде відображатися як Animesss
+    override val supportedTypes = setOf(TvType.Anime, TvType.AnimeMovie)
+    
+    override val hasMainPage = true 
+
+    // 1. Логіка пошуку аніме на сайті
+    override suspend fun search(query: String): List<SearchResponse> {
+        val url = "$mainUrl/?s=$query"
+        val html = app.get(url).text
+        val document = Jsoup.parse(html)
+
+        return document.select("article.post, div.anime-item, div.poster-card").map { element ->
+            val title = element.select("h2.title, a.title, h3").text()
+            val href = element.select("a").attr("href")
+            
+            val posterUrl = element.select("img").attr("data-src").ifEmpty {
+                element.select("img").attr("src")
+            }
+
+            newAnimeSearchResponse(title, href, TvType.Anime) {
+                this.posterUrl = posterUrl
+            }
+        }
+    }
+
+    // 2. Логіка завантаження сторінки тайтлу та формування списку серій
+    override suspend fun load(url: String): LoadResponse? {
+        val html = app.get(url).text
+        val document = Jsoup.parse(html)
+
+        val title = document.select("h1.entry-title, h1.title").text()
+        val poster = document.select("div.poster img, .anime-poster img, .single-poster img").attr("src")
+        val description = document.select("div.description, .entry-content, .video-description").text()
+
+        val episodes = mutableListOf<Episode>()
+        val episodeElements = document.select("ul.episodes-list li, div.series-list a, .episodes-nav a")
+        
+        if (episodeElements.isNotEmpty()) {
+            episodeElements.forEachIndexed { index, element ->
+                val epHref = element.attr("href").ifEmpty { url }
+                val epName = element.text().ifEmpty { "Серія ${index + 1}" }
+                
+                episodes.add(Episode(
+                    data = epHref,
+                    name = epName,
+                    episode = index + 1
+                ))
+            }
+        } else {
+            episodes.add(Episode(data = url, name = "Дивитися аніме (Перемикання серій у плеєрі)"))
+        }
+
+        return newAnimeLoadResponse(title, url, TvType.Anime) {
+            this.posterUrl = poster
+            this.plot = description
+            this.episodes = episodes
+        }
+    }
+
+    // 3. Логіка перехоплення плеєра Kodik та витягування відео
+    override suspend fun loadLinks(
+        data: String,
+        isCasting: Boolean,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ): Boolean {
+        val html = app.get(data).text
+        val document = Jsoup.parse(html)
+
+        val iframes = document.select("iframe")
+        var foundVideo = false
+
+        for (iframe in iframes) {
+            var iframeUrl = iframe.attr("src")
+            
+            if (iframeUrl.startsWith("//")) {
+                iframeUrl = "https:$iframeUrl"
+            }
+
+            if (iframeUrl.contains("kodik") || 
+                iframeUrl.contains("tomion.org") || 
+                iframeUrl.contains("kodikplayer.com") || 
+                iframeUrl.contains("aniqit")) {
+                
+                if (iframeUrl.contains("ls.kodikres.com")) continue
+
+                loadExtractor(iframeUrl, data, subtitleCallback, callback)
+                foundVideo = true
+            }
+        }
+
+        return foundVideo
+    }
+}
