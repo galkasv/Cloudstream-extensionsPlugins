4852db9cbb98f90ab5306af8391bbc58f0558965
galkasv <16668378+galkasv@users.noreply.github.com>
2026-07-04 19:32:21 +0300

    Update and rename AmdProvider.kt to TwitchProvider.kt

--- TwitchProvider/src/main/kotlin/recloudstream/TwitchProvider.kt
+++ TwitchProvider/src/main/kotlin/recloudstream/TwitchProvider.kt
@@ -5,24 +5,25 @@ import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.loadExtractor
 import org.jsoup.Jsoup
 
-class AmdProvider : MainAPI() { 
-    override var mainUrl = "https://amd.online" // Перевір, чи саме такий домен у браузері
-    override var name = "AMD.online" 
+// Назву класу залишаємо TwitchProvider, щоб працював TwitchPlugin.kt
+class TwitchProvider : MainAPI() { 
+    override var mainUrl = "https://amd.online" 
+    override var name = "AMD.online" // А в додатку назва буде красивою: AMD.online!
     override var supportedTypes = setOf(TvType.Anime, TvType.AnimeMovie) 
     override var hasMainPage = true 
 
     // 1. Пошук контенту на AMD.online
     override suspend fun search(query: String): List<SearchResponse> {
-        // Тимчасовий тестовий лінк для пошуку (підкоригуємо під структуру сайту)
         val url = "$mainUrl/?s=$query" 
         val html = app.get(url).text
         val document = Jsoup.parse(html)
 
-        // Тут будуть селектори конкретно для сайту AMD.online
-        return document.select("article, div.poster").map { element ->
-            val title = element.select("h2, a").text()
+        return document.select("article, div.poster, div.movie-item").map { element ->
+            val title = element.select("h2, a.title, h3").text()
             val href = element.select("a").attr("href")
-            val posterUrl = element.select("img").attr("src")
+            val posterUrl = element.select("img").attr("data-src").ifEmpty {
+                element.select("img").attr("src")
+            }
 
             newAnimeSearchResponse(title, href, TvType.Anime) {
                 this.posterUrl = posterUrl
@@ -36,12 +37,11 @@ class AmdProvider : MainAPI() {
         val document = Jsoup.parse(html)
 
         val title = document.select("h1").text()
-        val poster = document.select("img.poster").attr("src")
-        val description = document.select("div.description").text()
+        val poster = document.select("img.poster, div.poster img").attr("src")
+        val description = document.select("div.description, div.story").text()
 
         val episodesList = mutableListOf<Episode>()
         
-        // Базовий заглушковий варіант для однієї серії/плеєра
         episodesList.add(newEpisode(url) {
             this.name = "Дивитися на AMD.online"
         })
@@ -53,7 +53,7 @@ class AmdProvider : MainAPI() {
         }
     }
 
-    // 3. Пошук відео-посилань (іфреймів)
+    // 3. Пошук відео-посилань (плеєрів)
     override suspend fun loadLinks(
         data: String,
         isCasting: Boolean,
@@ -71,8 +71,14 @@ class AmdProvider : MainAPI() {
                 iframeUrl = "https:$iframeUrl"
             }
 
-            // Якщо вони теж використовують Kodik або інші стандартні плеєри:
-            if (iframeUrl.contains("kodik") || iframeUrl.contains("ashdi") || iframeUrl.contains("anitube")) {
+            if (iframeUrl.contains("kodik") || 
+                iframeUrl.contains("tomion.org") || 
+                iframeUrl.contains("kodikplayer.com") || 
+                iframeUrl.contains("aniqit") ||
+                iframeUrl.contains("ashdi")) {
+                
+                if (iframeUrl.contains("ls.kodikres.com")) continue
+
                 loadExtractor(iframeUrl, data, subtitleCallback, callback)
                 foundVideo = true
             }
