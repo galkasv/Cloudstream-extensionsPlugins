1d578efd460d49d295466b9766f994334e6e413a
galkasv <16668378+galkasv@users.noreply.github.com>
2026-07-05 22:38:00 +0300

    D8R8

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.7.3")
+        classpath("com.android.tools:r8:8.8.24")
         classpath("com.github.recloudstream.gradle:com.lagradost.cloudstream3.gradle.gradle.plugin:32895aedb6")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.0")
     }
