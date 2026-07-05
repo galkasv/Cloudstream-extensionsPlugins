efc4b9fc2e7c917fea046897e3ef11ebb89c7c68
galkasv <16668378+galkasv@users.noreply.github.com>
2026-07-05 15:27:45 +0300

    Feat: Rebranding to Animesss and AMDonline

--- AMDonlineProvider/build.gradle.kts
+++ AMDonlineProvider/build.gradle.kts
@@ -1,20 +1,16 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    id("com.lagradost.cloudstream3.gradle")
+}
+
 cloudstream {
     this.name = "AMD.online"
-    this.description = "AMD.online - СДонхуа та аніме"
+    this.description = "AMD.online - Серіали та аніме в українському дубляжі"
     this.authors = listOf("galkasv")
     this.language = "uk"
-
-    // Перенесли всередину блоку і захистили від конфліктів за допомогою this.
-    this.version = 2 
-
-    /**
-     * Status int as one of the following:
-     * 0: Down
-     * 1: Ok
-     * 2: Slow
-     * 3: Beta-only
-     **/
-    this.status = 1 
+    this.version = 2
+    this.status = 1
 
     this.tvTypes = listOf(
         "Anime", 
@@ -23,9 +19,6 @@ cloudstream {
         "TvSeries",
         "OVA"
     )
-    
-    // Автоматично підтягне гарну іконку сайту з фавікону Google
     this.iconUrl = "https://www.google.com/s2/favicons?domain=amd.online&sz=%size%"
-
     this.isCrossPlatform = true
 }
--- AnimesssProvider/build.gradle.kts
+++ AnimesssProvider/build.gradle.kts
@@ -1,20 +1,16 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    id("com.lagradost.cloudstream3.gradle")
+}
+
 cloudstream {
     this.name = "Animesss"
     this.description = "Animesss - Дивитися аніме та донхуа"
     this.authors = listOf("galkasv")
-    this.language = "uk" // Додає український прапор
-
-    // Перенесли всередину блоку і захистили від конфліктів за допомогою this.
-    this.version = 1 
-
-    /**
-     * Status int as one of the following:
-     * 0: Down
-     * 1: Ok
-     * 2: Slow
-     * 3: Beta-only
-     **/
-    this.status = 1 
+    this.language = "uk"
+    this.version = 1
+    this.status = 1
 
     this.tvTypes = listOf(
         "Anime", 
@@ -23,9 +19,6 @@ cloudstream {
         "TvSeries",
         "OVA"
     )
-    
-    // Автоматично підтягне гарну іконку сайту з фавікону Google
     this.iconUrl = "https://www.google.com/s2/favicons?domain=animesss.com&sz=%size%"
-
     this.isCrossPlatform = true
 }
