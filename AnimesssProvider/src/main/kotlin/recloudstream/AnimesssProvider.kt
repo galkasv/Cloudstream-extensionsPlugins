9528a6e4389a27c3cd3940ebeb15ea4ee7b23994
galkasv <16668378+galkasv@users.noreply.github.com>
2026-07-06 01:00:14 +0300

    delPlugin

--- AnimesssProvider/src/main/kotlin/recloudstream/AnimesssProvider.kt
+++ AnimesssProvider/src/main/kotlin/recloudstream/AnimesssProvider.kt
@@ -9,7 +9,7 @@ import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.TvType
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
-
+@CloudstreamPlugin
 class AnimesssProvider : MainAPI() {
     override var name = "Animesss"
     override var mainUrl = "https://animesss.com"
