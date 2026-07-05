3d1e94d0beb4ceb8df676f291cee2364099242a6
galkasv <16668378+galkasv@users.noreply.github.com>
2026-07-05 21:27:50 +0300

    Pluginkt

--- AnimesssProvider/src/main/kotlin/recloudstream/AnimesssPlugin.kt
+++ AnimesssProvider/src/main/kotlin/recloudstream/AnimesssPlugin.kt
@@ -1,13 +1,6 @@
 package recloudstream
 
-import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class AnimesssPlugin: Plugin() {
-    override fun load(context: Context) {
-        // Замість registerMainProvider тепер використовується registerProvider
-        registerProvider(AnimesssProvider()) 
-    }
-}
+class AnimesssPlugin
